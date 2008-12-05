f081a87cf78d41275f50841593985de2ceb04229
Ian Collins <ian@powerset.com>
2008-12-04 22:37:56 -0800

    updated brawndo

--- js/brawndo.js
+++ js/brawndo.js
@@ -18,7 +18,7 @@ Inspiration:
 
 var MooTools = {
 	'version': '1.2.1',
-	'build': '3a3a060aa500f4eacd6dc1b80db6921d6c2db1d3'
+	'build': '028e6fb28454d41a4d835ee988ef51655dc389e9'
 };
 
 var Native = function(options){
@@ -4724,10 +4724,62 @@ Element.implement({
 	get_transform_int: function(){
 		var tmp = this.getStyle('-webkit-transform')
 		if (!tmp.test(/matrix3d/) && tmp.match(/(\d+)/))
-			return tmp.match(/(\d+)/)[0].toInt()
+			return tmp.match(/(-?\d+)/)[0].toInt()
 		else 
 			return 0
 	}
 
 })
+
+/*
+Script: Hash.Cookie.js
+	Class for creating, reading, and deleting Cookies in JSON format.
+
+License:
+	MIT-style license.
+*/
+
+Hash.Cookie = new Class({
+
+	Extends: Cookie,
+
+	options: {
+		autoSave: true
+	},
+
+	initialize: function(name, options){
+		this.parent(name, options);
+		this.load();
+	},
+
+	save: function(){
+		var value = JSON.encode(this.hash);
+		if (!value || value.length > 4096) return false; //cookie would be truncated!
+		if (value == '{}') this.dispose();
+		else this.write(value);
+		return true;
+	},
+
+	load: function(){
+		this.hash = new Hash(JSON.decode(this.read(), true));
+		return this;
+	}
+
+});
+
+Hash.Cookie.implement((function(){
+
+	var methods = {};
+
+	Hash.each(Hash.prototype, function(method, name){
+		methods[name] = function(){
+			var value = method.apply(this.hash, arguments);
+			if (this.options.autoSave) this.save();
+			return value;
+		};
+	});
+
+	return methods;
+
+})());
 
